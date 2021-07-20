943f22e12df996544be069fae45d311c1bd86a4b
Justin Hager <justin_hager@swifttrans.com>
2021-07-20 11:20:22 -0700

    adds queries getActiveRange and getActiveYearsRange

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -1,9 +1,23 @@
-const Artist = require('../models/artist');
+import Artist from '../models/artist';
 
 /**
  * Finds the lowest and highest age of artists in the Artist collection
  * @return {promise} A promise that resolves with an object
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
-module.exports = () => {
+export default () => {
+  const minQuery = Artist.find({})
+    .sort({ age: 'asc' })
+    .limit(1)
+    .then((artist) => artist[0].age);
+  const maxQuery = Artist.find({})
+    .sort({ age: 'desc' })
+    .limit(1)
+    .then((artist) => artist[0].age);
+  return Promise.all([minQuery, maxQuery]).then((ages) => {
+    return {
+      min: ages[0],
+      max: ages[1],
+    };
+  });
 };
--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -1,9 +1,23 @@
-const Artist = require('../models/artist');
+import Artist from '../models/artist';
 
 /**
  * Finds the lowest and highest yearsActive of artists in the Artist collection
  * @return {promise} A promise that resolves with an object
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
-module.exports = () => {
+export default () => {
+  const minQuery = Artist.find({})
+    .sort({ yearsActive: 'asc' })
+    .limit(1)
+    .then((artist) => artist[0].yearsActive);
+  const maxQuery = Artist.find({})
+    .sort({ yearsActive: 'desc' })
+    .limit(1)
+    .then((artist) => artist[0].yearsActive);
+  return Promise.all([minQuery, maxQuery]).then((years) => {
+    return {
+      min: years[0],
+      max: years[1],
+    };
+  });
 };

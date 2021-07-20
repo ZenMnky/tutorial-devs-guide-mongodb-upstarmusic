1359dfe37447390ca0c3ef64d69e512c96580eac
Justin Hager <justin_hager@swifttrans.com>
2021-07-20 07:53:48 -0700

    init with models

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,12 @@
-// Todo: create Album Schema
+import mongoose from 'mongoose';
+
+const AlbumSchema = mongoose.Schema({
+  title: String,
+  date: Date,
+  copiesSold: Number,
+  numberTracks: Number,
+  image: String,
+  revenue: Number,
+});
+
+export default AlbumSchema;
--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,17 @@
-// Todo: Create Artist Model
+import mongoose from 'mongoose';
+import AlbumSchema from './album';
+
+const ArtistSchema = mongoose.Schema({
+  name: String,
+  age: Number,
+  yearsActive: Number,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  Albums: [AlbumSchema],
+});
+
+export default mongoose.model('Artist', ArtistSchema);
